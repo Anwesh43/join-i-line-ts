8059082bd921151e17e1b9ac3a1c707b69cc9091
Anwesh43 <anweshthecool0@gmail.com>
2020-11-12 23:15:15 +0530

    adding JLNode class

--- index.ts
+++ index.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class JILNode {
+
+    next : JILNode 
+    prev : JILNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new JILNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawJILNode(context, this.i, this.state.scale)
+    } 
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : JILNode {
+        var curr : JILNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
